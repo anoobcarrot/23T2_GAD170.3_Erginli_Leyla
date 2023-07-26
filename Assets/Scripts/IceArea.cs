68ab6dacfd422c2e3648017ed40102fca985a782
anoobcarrot <136556154+anoobcarrot@users.noreply.github.com>
2023-07-26 16:00:34 +1000

    Project 3
    - started project 3
    - worked on player character movement
    - worked on triggers

---
+++ Assets/Scripts/IceArea.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceArea : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("YOU ARE IN THE ICE ZONE!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 23T2_GAD170.3_Erginli_Leyla
+ Project 3
